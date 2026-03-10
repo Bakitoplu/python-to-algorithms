ba2c0634f96a0edf0fdd4eaa903cbbe4080711ca
Baki Toplu <11xybaki@gmail.com>
2026-03-10 15:54:03 +0300

    Day 02 - CS50P if, elif, or, and else

---
+++ compare.py
@@ -0,0 +1,31 @@
+x = int(input("X değeri nedir? "))
+y = int(input("Y değeri nedir? "))
+
+if x > y:
+    print("X Y den daha büyüktür")
+# if x < y:
+#     print("X Y den daha küçüktür")
+# if x == y:
+#     print("X Y ye eşittir")
+elif x < y:
+    print("X Y den daha küçüktür")
+# elif x == y:
+#     print("X Y ye eşittir")
+else:
+    print("X Y ye eşittir")
+
+print("-"*50)
+
+# if x < y or y > x:
+#     print("X Y ye eşit değildir")
+
+# else:
+#     print("X Y ye eşittir")
+
+# if x != y:
+#     print("X Y ye eşit değildir")
+if x == y:
+    print("X Y ye eşit değildir")
+
+else:
+    print("X Y ye eşittir")
