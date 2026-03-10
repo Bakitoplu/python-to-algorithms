459fd0094663d5de285884f8d60ce41cee42bdc6
Baki Toplu <11xybaki@gmail.com>
2026-03-10 16:12:14 +0300

    Day 02 - CS50P if, else puan hesaplama

---
+++ grade.py
@@ -0,0 +1,37 @@
+score = int(input("Puan: "))
+
+# if score >= 90 and score <=100:
+#     print("Seviye: A")
+# elif score >= 80 and score < 90:
+#     print("Seviye: B")
+# elif score >= 70 and score < 80:
+#     print("Seviye: C")
+# elif score >= 60 and score < 70:
+#     print("Seviye: D")
+# else:
+#     print("Seviye: F")
+
+
+# Aşırı önemli !!!
+if 90 <= score <=100:
+    print("Seviye: A")
+elif 80 <= score < 90:
+    print("Seviye: B")
+elif 70 <= score < 80:
+    print("Seviye: C")
+elif 60 <= score < 70:
+    print("Seviye: D")
+else:
+    print("Seviye: F")
+
+
+if score >=90:
+    print("Seviye: A")
+elif score >= 80:
+    print("Seviye: B")
+elif score >= 70:
+    print("Seviye: C")
+elif score >= 60:
+    print("Seviye: D")
+else:
+    print("Seviye: F")
